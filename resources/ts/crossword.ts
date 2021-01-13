aac4004faa171d844799a002d9cf2b0dc079ac6c
Alexander Rao <alexhrao@gmail.com>
2021-01-12 22:49:51 -0500

    Improve crossword

--- resources/ts/crossword.ts
+++ resources/ts/crossword.ts
@@ -179,6 +179,7 @@ window.onload = async () => {
     if (game === undefined) {
         return;
     }
+    const confetti = setupConfetti();
     const { liveBoxes, board } = setupBoard(game.board);
     const clueHolders = setupClues();
     const currClueViewer = convertClue();
@@ -313,6 +314,9 @@ window.onload = async () => {
         console.log(`Disable clue ${index} (${dir})`);
         liveClues[dir][index].viewer.classList.add('disabled');
         liveClues[dir][index].clue.done = true;
+        if (!confetti.isRunning() && puzzleCheck(false)) {
+            confetti.start();
+        }
     }
     function lookupClue(lb: LiveBox): ClueLookup;
     function lookupClue(row: number, col: number): ClueLookup;
@@ -445,6 +449,58 @@ window.onload = async () => {
         nextClue(backwards, true);
     }
 
+    const options = document.createElement('div');
+    options.id = 'options';
+    
+    const checkPuzzle = document.createElement('button');
+    checkPuzzle.textContent = 'Check Puzzle';
+    function puzzleCheck(shouldFlag = true): boolean {
+        let allTrue = true;
+        liveBoxes
+            .flat()
+            .forEach(lb => {
+                if (lb.input !== undefined && lb.box !== null && typeof lb.box !== 'boolean') {
+                    if (lb.input.value.length === 0) {
+                        allTrue = false;
+                    } else if (lb.box.answer !== lb.input.value) {
+                        if (shouldFlag) {
+                            lb.container.classList.add('incorrect');
+                        }
+                        allTrue = false;
+                    } else {
+                        if (shouldFlag) {
+                            lb.container.classList.remove('incorrect');
+                        }
+                    }
+                }
+            });
+        return allTrue;
+    }
+    checkPuzzle.onclick = () => puzzleCheck();
+    const revealPuzzle = document.createElement('button');
+    revealPuzzle.textContent = 'Reveal Puzzle';
+    revealPuzzle.onclick = () => {
+        liveBoxes
+            .flat()
+            .forEach(lb => {
+                if (lb.input !== undefined && lb.box !== null && typeof lb.box !== 'boolean') {
+                    lb.container.classList.remove('incorrect');
+                    lb.input.value = lb.box.answer;
+                }
+            });
+        clueList.forEach(lc => disableClue(lc.clue.index, lc.clue.dir));
+    };
+
+    const resetPuzzle = document.createElement('button');
+    resetPuzzle.textContent = 'Reset Puzzle';
+    resetPuzzle.onclick = () => {
+        window.location.reload();
+    };
+
+    options.appendChild(checkPuzzle);
+    options.appendChild(revealPuzzle);
+    options.appendChild(resetPuzzle);
+
     liveBoxes
         .flat()
         .forEach(lb => {
@@ -489,7 +545,6 @@ window.onload = async () => {
                 }
                 input.onkeydown = (e: KeyboardEvent) => {
                     if (e.key === 'Tab') {
-                        console.log('TAB');
                         e.preventDefault();
                         nextClue(e.shiftKey);
                     }
@@ -574,17 +629,20 @@ window.onload = async () => {
                                 liveBoxes[lb.row - 1][lb.col].input?.focus();
                             }
                         }
+                        lb.container.classList.remove('incorrect');
                     } else if (lb.input?.value.length === 0) {
                         lb.wasFilled = false;
                     }
                     if (!/^[a-zA-Z]{1}$/.test(input.value)) {
                         input.value = '';
+                        lb.container.classList.remove('incorrect');
                         return;
                     }
                     if (!/^[a-zA-Z]{1}$/.test(e.key)) {
                         return;
                     }
                     if (input.value.length === 1) {
+                        lb.container.classList.remove('incorrect');
                         lb.wasFilled = true;
                         // check if anyone, down or accross, is finished
                         const possible = lookupClue(lb);
@@ -672,10 +730,245 @@ window.onload = async () => {
     
     container.appendChild(board);
     container.appendChild(currClueViewer.viewer);
-    container.appendChild(clueHolders.clueContainer);
+    const clueContainer = document.createElement('div');
+    clueContainer.id = 'clueAndOptions';
+    clueContainer.appendChild(options);
+    clueContainer.appendChild(clueHolders.clueContainer);
+    container.appendChild(clueContainer);
     document.body.appendChild(container);
     const firstClue = game.clues[0];
     const { row: firstRow, col: firstCol } = liveClues[firstClue.dir][firstClue.index];
     liveBoxes[firstRow][firstCol].input?.focus();
     highlightClue(firstRow, firstCol, firstClue.dir);
 }
+
+interface Particle {
+    color: string;
+    color2: string;
+    x: number;
+    y: number;
+    diameter: number;
+    tilt: number;
+    tiltAngleIncrement: number;
+    tiltAngle: number;
+}
+
+interface Confetti {
+    maxCount: number;
+    speed: number;
+    frameInterval: number;
+    alpha: number;
+    gradient: boolean;
+    start: () => void;
+    stop: () => void;
+    toggle: () => void;
+    pause: () => void;
+    resume: () => void;
+    togglePause: () => void;
+    remove: () => void;
+    isPaused: () => boolean;
+    isRunning: () => boolean;
+}
+
+function setupConfetti() {
+    const confetti: Confetti = {
+        maxCount: 150,      //set max confetti count
+        speed: 2,           //set the particle animation speed
+        frameInterval: 15,  //the confetti animation frame interval in milliseconds
+        alpha: 1.0,         //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
+        gradient: false,    //whether to use gradients for the confetti particles
+        start: startConfetti,        //call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
+        stop: stopConfetti,         //call to stop adding confetti
+        toggle: toggleConfetti,       //call to start or stop the confetti animation depending on whether it's already running
+        pause: pauseConfetti,        //call to freeze confetti animation
+        resume: resumeConfetti,       //call to unfreeze confetti animation
+        togglePause: toggleConfettiPause,  //call to toggle whether the confetti animation is paused
+        remove: removeConfetti,       //call to stop the confetti animation and remove all confetti immediately
+        isPaused: isConfettiPaused,     //call and returns true or false depending on whether the confetti animation is paused
+        isRunning: isConfettiRunning	    //call and returns true or false depending on whether the animation is running
+    };
+    const supportsAnimationFrame = window.requestAnimationFrame ?? window.webkitRequestAnimationFrame;
+    const colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
+    let streamingConfetti = false;
+    let pause = false;
+    let lastFrameTime = Date.now();
+    let particles: Particle[] = [];
+    let waveAngle = 0;
+    let context: CanvasRenderingContext2D|null = null;
+    let innerWidth = document.documentElement.scrollWidth;
+    let innerHeight = document.documentElement.scrollHeight;
+
+    function resetParticle(particle: Particle, width: number, height: number): Particle {
+        particle.color = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+        particle.color2 = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+        particle.x = Math.random() * width;
+        particle.y = Math.random() * height - height;
+        particle.diameter = Math.random() * 10 + 5;
+        particle.tilt = Math.random() * 10 - 10;
+        particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
+        particle.tiltAngle = Math.random() * Math.PI;
+        return particle;
+    }
+
+    function toggleConfettiPause() {
+        if (pause)
+            resumeConfetti();
+        else
+            pauseConfetti();
+    }
+
+    function isConfettiPaused() {
+        return pause;
+    }
+
+    function pauseConfetti() {
+        pause = true;
+    }
+
+    function resumeConfetti() {
+        pause = false;
+        runAnimation();
+    }
+
+    function runAnimation() {
+        if (pause) {
+            return;
+        } else if (particles.length === 0) {
+            context?.clearRect(0, 0, innerWidth, innerHeight);
+        } else {
+            const now = Date.now();
+            const delta = now - lastFrameTime;
+            if ((!supportsAnimationFrame || delta > confetti.frameInterval) && context !== null) {
+                context?.clearRect(0, 0, innerWidth, innerHeight);
+                updateParticles();
+                drawParticles(context);
+                lastFrameTime = now - (delta % confetti.frameInterval);
+            }
+            requestAnimationFrame(runAnimation);
+        }
+    }
+
+    function startConfetti(timeout?: number, min?: number, max?: number) {
+        const animator = () => {
+            return window.requestAnimationFrame
+                ?? window.webkitRequestAnimationFrame
+                ?? (callback => window.setTimeout(callback, confetti.frameInterval));
+        }
+        innerWidth = document.documentElement.scrollWidth*0.99;
+        innerHeight = document.documentElement.scrollHeight;
+        window.requestAnimationFrame = animator();
+        let canvas = document.querySelector<HTMLCanvasElement>('#confettiCanvas');
+        if (canvas === null) {
+            canvas = document.createElement("canvas");
+            canvas.setAttribute("id", "confettiCanvas");
+            document.body.prepend(canvas);
+            canvas.width = innerWidth;
+            canvas.height = innerHeight;
+            console.log(innerHeight);
+            window.addEventListener("resize", () => {
+                innerWidth = document.documentElement.scrollWidth;
+                innerHeight = document.documentElement.scrollHeight
+                if (canvas !== null) {
+                    canvas.width = innerWidth;
+                    canvas.height = innerHeight;
+                }
+            }, true);
+            context = canvas.getContext("2d");
+        } else if (context === null) {
+            context = canvas.getContext("2d");
+        }
+        let count = confetti.maxCount;
+        if (min) {
+            if (max) {
+                if (min === max) {
+                    count = particles.length + max;
+                } else {
+                    if (min > max) {
+                        const temp = min;
+                        min = max;
+                        max = temp;
+                    }
+                    count = particles.length + ((Math.random() * (max - min) + min) | 0);
+                }
+            } else {
+                count = particles.length + min;
+            }
+        } else if (max) {
+            count = particles.length + max;
+        }
+        while (particles.length < count) {
+            particles.push(resetParticle({} as Particle, innerWidth, innerHeight));
+        }
+        streamingConfetti = true;
+        pause = false;
+        runAnimation();
+        if (timeout) {
+            window.setTimeout(stopConfetti, timeout);
+        }
+    }
+
+    function stopConfetti() {
+        streamingConfetti = false;
+    }
+
+    function removeConfetti() {
+        stop();
+        pause = false;
+        particles = [];
+    }
+
+    function toggleConfetti() {
+        if (streamingConfetti) {
+            stopConfetti();
+        } else {
+            startConfetti();
+        }
+    }
+    
+    function isConfettiRunning() {
+        return streamingConfetti;
+    }
+
+    function drawParticles(context: CanvasRenderingContext2D) {
+        particles.forEach(particle => {
+            context.beginPath();
+            context.lineWidth = particle.diameter;
+            const x2 = particle.x + particle.tilt;
+            const x = x2 + particle.diameter / 2;
+            const y2 = particle.y + particle.tilt + particle.diameter / 2;
+            if (confetti.gradient) {
+                const gradient = context.createLinearGradient(x, particle.y, x2, y2);
+                gradient.addColorStop(0, particle.color);
+                gradient.addColorStop(1.0, particle.color2);
+                context.strokeStyle = gradient;
+            } else
+                context.strokeStyle = particle.color;
+            context.moveTo(x, particle.y);
+            context.lineTo(x2, y2);
+            context.stroke();
+        })
+    }
+
+    function updateParticles() {
+        waveAngle += 0.01;
+        particles.forEach((particle, i) => {
+            if (!streamingConfetti && particle.y < -15) {
+                particle.y = innerHeight + 100;
+            } else {
+                particle.tiltAngle += particle.tiltAngleIncrement;
+                particle.x += Math.sin(waveAngle) - 0.5;
+                particle.y += (Math.cos(waveAngle) + particle.diameter + confetti.speed) * 0.5;
+                particle.tilt = Math.sin(particle.tiltAngle) * 15;
+            }
+            if (particle.x > innerWidth + 20 || particle.x < -20 || particle.y > innerHeight) {
+                if (streamingConfetti && particles.length <= confetti.maxCount) {
+                    resetParticle(particle, innerWidth, innerHeight);
+                } else {
+                    particles.splice(i, 1);
+                }
+            }
+        });
+    }
+
+    return confetti;
+};
